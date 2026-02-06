5043cc031700a2ea59de6dfe15aa20b91e1c387e
Lucas Blancas <lablancas@gmail.com>
2026-02-05 22:35:48 -0600

    initial commit

---
+++ src/collections/Users.ts
@@ -0,0 +1,17 @@
+import type { CollectionConfig } from 'payload'
+
+export const Users: CollectionConfig = {
+  slug: 'users-payload',
+  labels: {
+    singular: 'User',
+    plural: 'Users',
+  },
+  admin: {
+    useAsTitle: 'email',
+  },
+  auth: true,
+  fields: [
+    // Email added by default
+    // Add more fields as needed
+  ],
+}
